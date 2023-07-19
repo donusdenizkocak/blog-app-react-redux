9714613e388d719b3fda9a8d2273bb5527206a10
denizderK <denizderya8668@gmail.com>
2023-07-20 00:06:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
+import store, { persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
